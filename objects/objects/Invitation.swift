324b010a734c5145309caa92a9f5ba960ef8f267
JiWon Lee <jiwon.swdev@gmail.com>
2021-05-21 16:11:26 +0900

    feat: 초대장 클래스 구현

---
+++ objects/objects/Invitation.swift
@@ -0,0 +1,20 @@
+//
+//  Invitation.swift
+//  objects
+//
+//  Created by 이지원 on 2021/05/21.
+//
+
+import Foundation
+
+/**
+이벤트 당첨자에게 발송되는 초대장
+ - when: 공연을 관람할 수 있는 초대일자
+ */
+class Invitation {
+    private let when: Date
+
+    init(when: Date) {
+        self.when = when
+    }
+}
