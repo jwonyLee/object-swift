2ee5323084bc36c7e253ef62535b43316d529ac7
JiWon Lee <jiwon.swdev@gmail.com>
2021-05-28 20:18:16 +0900

    refactor: 예매 요금 계산 방식 변경

--- objects/objects/chapter04-movie-for-data/ReservationAgency.swift
+++ objects/objects/chapter04-movie-for-data/ReservationAgency.swift
@@ -13,40 +13,7 @@ import Foundation
 class ReservationAgency {
 
     func reserve(screening: Screening_, customer: Customer, audienceCount: Int) -> Reservation_ {
-        let movie: Movie_ = screening.getMovie()
-
-        var discountable: Bool = false
-        for condition in movie.getDiscountConditions() {
-            if condition.getType() == DiscountConditionType.period {
-                discountable = screening.getWhenScreened().getDayOfWeek() == condition.getDayOfWeek() &&
-                    condition.getStartTime() <= screening.getWhenScreened() &&
-                    condition.getEndTime() >= screening.getWhenScreened()
-            } else {
-                discountable = condition.getSequence() == screening.getSequence()
-            }
-
-            if discountable {
-                break
-            }
-        }
-
-        var fee: Money = Money(amount: 0)
-        if discountable {
-            var discountAmount: Money = Money.zero
-            switch movie.getMovieType() {
-            case .amountDiscount:
-                discountAmount = movie.getDiscountAmount()
-            case .percentDiscount:
-                discountAmount = movie.getFee().times(percent: movie.getDiscountPercent())
-            case .noneDiscount:
-                discountAmount = Money.zero
-            }
-
-            fee = movie.getFee().minus(amount: discountAmount)
-        } else {
-            fee = movie.getFee()
-        }
-
+        let fee = screening.calculateFee(audienceCount: audienceCount)
         return Reservation_(customer: customer, screening: screening, fee: fee, audienceCount: audienceCount)
     }
 }
