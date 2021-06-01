ffd53788156a8e16aff895a215d250f4ea7ff02b
JiWon Lee <jiwon.swdev@gmail.com>
2021-06-01 15:19:41 +0900

    feat: SequenceCondition, PercentCondition 기능 구현

--- objects/objects/chapter05-GRASP/Chapter05.playground/Sources/PeriodCondition.swift
+++ objects/objects/chapter05-GRASP/Chapter05.playground/Sources/PeriodCondition.swift
@@ -1,7 +1,19 @@
 import Foundation
 
 class PeriodCondition: DiscountCondition {
+    private let dayOfWeek: DayOfWeek
+    private let startTime: Date
+    private let endTime: Date
+
+    init(dayOfWeek: DayOfWeek, startTime: Date, endTime: Date) {
+        self.dayOfWeek = dayOfWeek
+        self.startTime = startTime
+        self.endTime = endTime
+    }
+
     func isSatisfiedBy(screening: Screening) -> Bool {
-        <#code#>
+        return dayOfWeek == screening.getWhenScreened().getDayOfWeek() &&
+            startTime <= screening.getStartTime() &&
+            endTime >= screening.getStartTime()
     }
 }
--- objects/objects/chapter05-GRASP/Chapter05.playground/Sources/SequenceCondition.swift
+++ objects/objects/chapter05-GRASP/Chapter05.playground/Sources/SequenceCondition.swift
@@ -1,7 +1,13 @@
 import Foundation
 
 class SequenceCondition: DiscountCondition {
+    private let sequence: Int
+
+    init(sequence: Int) {
+        self.sequence = sequence
+    }
+
     func isSatisfiedBy(screening: Screening) -> Bool {
-        <#code#>
+        return sequence == screening.getSequence()
     }
 }
