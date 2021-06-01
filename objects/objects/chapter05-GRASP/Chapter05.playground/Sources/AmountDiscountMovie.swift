7d05ab54187569a672a848eb32e98a5984563be3
JiWon Lee <jiwon.swdev@gmail.com>
2021-06-01 15:23:16 +0900

    chore: super.init() 호출 순서 변경
    super.init() 호출 이전에 초기화 해야 함

--- objects/objects/chapter05-GRASP/Chapter05.playground/Sources/AmountDiscountMovie.swift
+++ objects/objects/chapter05-GRASP/Chapter05.playground/Sources/AmountDiscountMovie.swift
@@ -4,8 +4,8 @@ class AmountDiscountMovie: Movie {
     private let discountAmount: Money
 
     init(title: String, runningTime: TimeInterval, fee: Money, discountAmount: Money, discountConditions: [DiscountCondition]) {
-        super.init(title: title, runningTime: runningTime, fee: fee, discountConditions: discountConditions)
         self.discountAmount = discountAmount
+        super.init(title: title, runningTime: runningTime, fee: fee, discountConditions: discountConditions)
     }
 
     override func calculateDiscountAmount() -> Money {
--- objects/objects/chapter05-GRASP/Chapter05.playground/Sources/PercentDiscountMovie.swift
+++ objects/objects/chapter05-GRASP/Chapter05.playground/Sources/PercentDiscountMovie.swift
@@ -4,8 +4,8 @@ class PercentDiscountMovie: Movie {
     private let percent: Double
 
     init(title: String, runningTime: TimeInterval, fee: Money, percent: Double, discountConditions: [DiscountCondition]) {
-        super.init(title: title, runningTime: runningTime, fee: fee, discountConditions: discountConditions)
         self.percent = percent
+        super.init(title: title, runningTime: runningTime, fee: fee, discountConditions: discountConditions)
     }
 
     override func calculateDiscountAmount() -> Money {
