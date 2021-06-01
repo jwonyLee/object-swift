17c366a4fff266611e9fdf1460309c0afaecbf71
JiWon Lee <jiwon.swdev@gmail.com>
2021-06-01 16:06:31 +0900

    refactor: 메서드 호출 주체 변경

--- objects/objects/chapter05-GRASP/Chapter05.playground/Sources/ReservationAgency.swift
+++ objects/objects/chapter05-GRASP/Chapter05.playground/Sources/ReservationAgency.swift
@@ -3,12 +3,13 @@ import Foundation
 class ReservationAgency {
     func reserve(screening: Screening, customer: Customer, audienceCount: Int) {
         let discountable: Bool = checkDiscountable(screening: screening)
-        let fee: Money = calculateFee(screening: screening, )
+        let fee: Money = calculateFee(screening: screening, discountable: discountable, audienceCount: audienceCount)
+        return createReservation(screening: screening, customer: customer, audienceCount: audienceCount, fee: fee)
     }
 
     private func checkDiscountable(screening: Screening) -> Bool {
         return screening.getMovie().getDiscountConditions()
-            .map { isDiscountable(condition: $0, screening: screening)}
+            .map { $0.isDiscountable(screening: screening) }
             .count >= 1
     }
 
