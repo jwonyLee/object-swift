3ae80e0807ceac7fc5b433a3b4c3bcac8c6de230
JiWon Lee <jiwon.swdev@gmail.com>
2021-06-06 11:31:52 +0900

    feat: 티켓 판매 도메인 복제

---
+++ objects/objects/chapter06-Demeter/Chapter06.playground/Sources/TicketOffice.swift
@@ -0,0 +1,35 @@
+//
+//  TicketOffice.swift
+//  objects
+//
+//  Created by 이지원 on 2021/05/21.
+//
+
+import Foundation
+
+/**
+ 매표소
+ - amount: 판매 금액
+ - tickets: 판매하거나 교환해 줄 티켓의 목록
+ */
+class TicketOffice {
+    private var amount: Double
+    private var tickets: [Ticket] = []
+
+    init(amount: Double, tickets: [Ticket]) {
+        self.amount = amount
+        self.tickets = tickets
+    }
+
+    func getTicket() -> Ticket {
+        return tickets.removeFirst()
+    }
+
+    func minusAmount(_ amount: Double) {
+        self.amount -= amount
+    }
+
+    func plusAmount(_ amount: Double) {
+        self.amount += amount
+    }
+}
