98fab6532778a2c77935de835c3f74388c143133
JiWon Lee <jiwon.swdev@gmail.com>
2021-06-06 11:39:19 +0900

    refactor: Theater 클래스가 디미터 법칙을 준수하도록 수정

--- objects/objects/chapter06-Demeter/Chapter06.playground/Sources/TicketSeller.swift
+++ objects/objects/chapter06-Demeter/Chapter06.playground/Sources/TicketSeller.swift
@@ -21,4 +21,16 @@ class TicketSeller {
     func getTicketOffice() -> TicketOffice {
         return ticketOffice
     }
+
+    func setTicket(audience: Audience) {
+        if audience.getBag().hasInvitation() {
+            let ticket: Ticket = ticketOffice.getTicket()
+            audience.getBag().setTicket(ticket)
+        } else {
+            let ticket: Ticket = ticketOffice.getTicket()
+            audience.getBag().minusAmount(ticket.getFee())
+            ticketOffice.plusAmount(ticket.getFee())
+            audience.getBag().setTicket(ticket)
+        }
+    }
 }
