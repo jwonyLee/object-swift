2b1d6de8dad3845e8acc36a765ee30fd761bb1e1
JiWon Lee <jiwon.swdev@gmail.com>
2021-06-06 14:10:43 +0900

    feat: 이벤트 클래스 명령-쿼리 분리 구현
    - 자바 코드를 스위프트로 구현하는 방법을 모르겠어서 주석으로 원본 코드 작성

--- objects/objects/chapter06-Demeter/Chapter06.playground/Sources/event/Event.swift
+++ objects/objects/chapter06-Demeter/Chapter06.playground/Sources/event/Event.swift
@@ -8,12 +8,32 @@ import Foundation
  */
 public class Event {
     private let subject: String
-    private let from: Date
+    private var from: Date
     private let duration: TimeInterval
 
     public init(subject: String, from: Date, duration: TimeInterval) {
         self.subject = subject
         self.from = from
         self.duration = duration
+    }
+
+    public func isSatisfied(schedule: RecurringSchedule) -> Bool {
+        if from.getDayOfWeek() != schedule.getDayOfWeek() ||
+            from == schedule.getFrom() ||
+            duration == schedule.getDuration() {
+            return false
+        }
+        return true
+    }
+
+    public func reschedule(_ schedule: RecurringSchedule) {
+        /* java code
+        from = LocalDateTime.of(from.toLocalDate().plusDays(daysDistance(schedule)), schedule.getFrom())
+        duration = schedule.getDuration()
+         */
+    }
+
+    private func daysDistance(schedule: RecurringSchedule) -> Int {
+        return schedule.getDayOfWeek().rawValue - from.getDayOfWeek().rawValue
     }
 }
