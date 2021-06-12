c874a8ab57a10962d648f1ba9ee5aefefd7f259f
JiWon Lee <jiwon.swdev@gmail.com>
2021-06-12 14:31:15 +0900

    feat: 아르바이트 직원 추가
    - 아르바이트 직원 추가
    - 아르바이트 직원인지 여부 판별 기능 추가
    - 아르바이트 직원 급여 계산 기능 추가
    - 아르바이트 직원 추가로 인한 변경사항 구현

--- objects/objects/chapter07-pay/main.swift
+++ objects/objects/chapter07-pay/main.swift
@@ -1,9 +1,13 @@
 import Foundation
 
 /// 직원 목록
-let employees = ["직원A", "직원B", "직원C"]
+let employees = ["직원A", "직원B", "직원C", "아르바이트D", "아르바이트E", "아르바이트F"]
 /// 기본급
-let basePays = [400, 300, 250]
+let basePays: [Double] = [400, 300, 250, 1, 1, 1.5]
+/// 아르바이트 직원인지 여부
+let hourlys = [false, false, false, true, true, true]
+/// 한달 간의 업무 누적 시간: 정규 직원의 경우 0
+let timeCards: [Double] = [0, 0, 0, 120, 120, 120]
 
 enum Operations {
     case pay
@@ -20,9 +24,18 @@ func main(operation: Operations, name: String = "") {
     }
 }
 
+func isHourly(name: String) -> Bool {
+    return hourlys[employees.firstIndex(of: name)!]
+}
+
 func calculatePay(name: String) {
     let taxRate = getTaxRate()
-    let pay = calculatePayFor(name: name, taxRate: taxRate)
+    var pay: Double = 0
+    if isHourly(name: name) {
+        pay = calculateHourlyPayFor(name: name, taxRate: taxRate)
+    } else {
+        pay = calculatePayFor(name: name, taxRate: taxRate)
+    }
     print(describeResult(name: name, pay: pay))
 }
 
@@ -40,6 +53,13 @@ func calculatePayFor(name: String, taxRate: Double) -> Double {
     return basePay - (basePay * taxRate)
 }
 
+// 아르바이트 직원의 급여를 계산한다.
+func calculateHourlyPayFor(name: String, taxRate: Double) -> Double {
+    let index = employees.firstIndex(of: name)!
+    let basePay = Double(basePays[index]) * timeCards[index]
+    return basePay - (basePay * taxRate)
+}
+
 /// 양식에 맞게 결과를 출력한다.
 func describeResult(name: String, pay: Double) -> String {
     return "이름: \(name), 급여: \(pay)"
@@ -47,9 +67,11 @@ func describeResult(name: String, pay: Double) -> String {
 
 /// 회사에 속한 모든 직원들의 기본급의 총합을 구한다.
 func sumOfBasePays() {
-    var result = 0
-    for basePay in basePays {
-        result += basePay
+    var result: Double = 0
+    for name in employees {
+        if !isHourly(name: name) {
+            result += basePays[employees.firstIndex(of: name)!]
+        }
     }
     print(result)
 }
